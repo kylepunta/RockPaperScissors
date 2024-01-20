3a86faf189d3bd9674676214a0cc92401896b6c5
kylepunta <kylepurcell1998@gmail.com>
2024-01-20 15:02:37 +0000

    Added random number generator and if statements to assign computerChoice a string literal

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 function getComputerChoice(){
-    
+
+    const ROCK = 1; 
+    const PAPER = 2;
+    const SCISSORS = 3; // rock, paper, and scissors are assigned 1, 2, and 3 respectively
+
+    let computerChoice = Math.floor((Math.random() * 10) + 1);
+    // computerChoice is assigned a randomly generated number between 1 and 10
+    while (computerChoice != 1 && computerChoice != 2 && computerChoice != 3){
+    // this ensures the number generated is between 1 and 3
+        computerChoice = Math.floor((Math.random() * 10) + 1);
+    }
+
+    if (computerChoice === rock){
+        computerChoice = "rock"; // computerChoice is assigned rock 
+    } else if (computerChoice === paper){
+        computerChoice = "paper"; // computerChoice is assigned paper
+    } else if (computerChoice === scissors){
+        computerChoice = "scissors"; // computerChoice is assigned scissors
+    }
+
+    return computerChoice; // computerChoice's string value is returned
 }
+
+console.log(getComputerChoice()); // returns computerChoice into the console
