2d18f2d02c29b17ec9b262aa8755f6c082e19c77
kylepunta <kylepurcell1998@gmail.com>
2024-01-20 15:08:39 +0000

    Removed constant variables

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 function getComputerChoice(){
-
-    const ROCK = 1; 
-    const PAPER = 2;
-    const SCISSORS = 3; // rock, paper, and scissors are assigned 1, 2, and 3 respectively
-
+    
     let randomNumber = Math.floor((Math.random() * 10) + 1);
     // computerChoice is assigned a randomly generated number between 1 and 10
     while (randomNumber != 1 && randomNumber != 2 && randomNumber != 3){
