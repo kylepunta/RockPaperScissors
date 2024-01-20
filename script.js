6cdc8857ed8891799af56cd85da77df67a0b7dfe
kylepunta <kylepurcell1998@gmail.com>
2024-01-20 15:09:34 +0000

    Capitalized the string literals

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ function getComputerChoice(){
     }
 
     if (randomNumber === 1){
-        computerChoice = "rock"; // computerChoice is assigned rock 
+        computerChoice = "Rock"; // computerChoice is assigned rock 
     } else if (randomNumber === 2){
-        computerChoice = "paper"; // computerChoice is assigned paper
+        computerChoice = "Paper"; // computerChoice is assigned paper
     } else if (randomNumber === 3){
-        computerChoice = "scissors"; // computerChoice is assigned scissors
+        computerChoice = "Scissors"; // computerChoice is assigned scissors
     }
 
     return computerChoice; // computerChoice's string value is returned
