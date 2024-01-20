904957f0a86ebc9523177c48acdd883db23ec960
kylepunta <kylepurcell1998@gmail.com>
2024-01-20 15:04:31 +0000

    Added randomNumber variable and assigned it with a random number generator

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ function getComputerChoice(){
     const PAPER = 2;
     const SCISSORS = 3; // rock, paper, and scissors are assigned 1, 2, and 3 respectively
 
-    let computerChoice = Math.floor((Math.random() * 10) + 1);
+    let randomNumber = Math.floor((Math.random() * 10) + 1);
     // computerChoice is assigned a randomly generated number between 1 and 10
-    while (computerChoice != 1 && computerChoice != 2 && computerChoice != 3){
+    while (randomNumber != 1 && randomNumber != 2 && randomNumber != 3){
     // this ensures the number generated is between 1 and 3
-        computerChoice = Math.floor((Math.random() * 10) + 1);
+        randomNumber = Math.floor((Math.random() * 10) + 1);
     }
 
-    if (computerChoice === rock){
+    if (randomNumber === rock){
         computerChoice = "rock"; // computerChoice is assigned rock 
-    } else if (computerChoice === paper){
+    } else if (randomNumber === paper){
         computerChoice = "paper"; // computerChoice is assigned paper
-    } else if (computerChoice === scissors){
+    } else if (randomNumber === scissors){
         computerChoice = "scissors"; // computerChoice is assigned scissors
     }
 
